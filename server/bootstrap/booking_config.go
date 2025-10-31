3fd2a4d4b45464310fe7aeca1699b0c3d37d17fd
pakutonzz <patkultornchairat@gmail.com>
2025-11-01 06:38:52 +0700

    refactor: Simplify BookingConfig. Set DailyLimit: 2, HoldTTL: 30 min QRTokenTTL: 30 min

--- server/bootstrap/booking_config.go
+++ server/bootstrap/booking_config.go
@@ -1,28 +1,14 @@
 package bootstrap
 
 import (
-	"os"
-	"strconv"
-	"time"
-
-	"github.com/RathaTart/FoodBridge/pkg/booking"
+    "time"
+    "github.com/RathaTart/FoodBridge/pkg/booking"
 )
 
-// BookingConfig reads env and returns a shared config for all booking services.
 func BookingConfig() booking.Config {
-	limit := 2 // change default to 2
-	if v := os.Getenv("BOOKING_DAILY_LIMIT"); v != "" {
-		if n, err := strconv.Atoi(v); err == nil && n >= 0 {
-			limit = n
-		}
-	}
-	loc, err := time.LoadLocation("Asia/Bangkok")
-	if err != nil {
-		loc = time.Local
-	}
-	return booking.Config{
-		DailyLimit: limit,
-		DayTZ:      loc,
-		// optionally set HoldTTL, QRTokenTTL, QRSecret here too
-	}
+    return booking.Config{
+        DailyLimit: 4,
+        HoldTTL:    30 * time.Minute,
+        QRTokenTTL: 30 * time.Minute,
+    }
 }
